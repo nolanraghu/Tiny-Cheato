ac667f6550daa690b4e8d6535e0458976735d357
Nolan R <kickquackstudio@gmail.com>
2020-08-12 15:23:10 -0400

    Initial Commit

---
+++ Tiny Cheato/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tiny Cheato
+//
+//  Created by Nolan Raghu on 8/12/20.
+//  Copyright © 2020 Nolan Raghu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
